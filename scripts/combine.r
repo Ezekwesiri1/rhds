bdf8f0100d734eb92de1658eb0a609bf7525edbb
Ezekwesiri1 <ezekwesirinwanosike@gmail.com>
2026-02-04 11:03:17 +0000

    new stuff

---
+++ scripts/combine.r
@@ -0,0 +1,55 @@
+library(rhds.rpackage)
+
+args <- commandArgs(trailingOnly = T)
+datadir <- args[1]
+resultsdir <- args[2]
+
+pred.protein.filename <- file.path(resultsdir, "predicted-proteins.txt")
+clinical.filename <- file.path(resultsdir, "clinical-clean.txt")
+
+pred.proteins <- read.table(pred.protein.filename,
+  header = T, sep = "\t", stringsAsFactors = F
+)
+
+## extract participant tissue information
+tissues <- data.frame(
+  participant = extract.participant(rownames(pred.proteins)),
+  tissue = extract.tissue(rownames(pred.proteins)),
+  participant.tissue = paste(extract.participant(rownames(pred.proteins)),
+    extract.tissue(rownames(pred.proteins)),
+    sep = "-"
+  )
+)
+tissues <- subset(tissues, tissue != "06" & tissue != "V582")
+
+## update pred.proteins to use participant.tissue rownames
+samples <- rownames(pred.proteins)
+rownames(pred.proteins) <- paste(extract.participant(samples),
+  extract.tissue(samples),
+  sep = "-"
+)
+
+## get cleaned clinical data
+clinical <- read.table(clinical.filename,
+  header = T, sep = "\t", stringsAsFactors = F
+)
+
+## combine with participant tissue info from predicted protein dataset
+clinical <- merge(clinical, tissues, by.x = "participant")
+clinical$tumor.or.normal <- ifelse(as.numeric(clinical$tissue) < 9, "tumor", "normal")
+clinical$tumor <- sign(clinical$tumor.or.normal == "tumor")
+
+
+table(rownames(pred.proteins) %in% clinical$participant.tissue)
+
+## combine the clinical info with the methylation predicted protein abundances
+out <- cbind(
+  clinical,
+  pred.proteins[match(clinical$participant.tissue, rownames(pred.proteins)), ]
+)
+
+## export results
+my.write.table(
+  out,
+  file.path(resultsdir, "combined-clin-pred-proteins.txt")
+)
